4381c6f4b75def4875890411a1d7f3809f082217
Unknown <SylwekMuzyka@gmail.com>
2017-05-24 13:05:35 +0200

    android first

---
+++ AndroidHotelBooking/app/src/main/java/pl/androidhotelbooking/RoomsActivity.java
@@ -0,0 +1,15 @@
+package pl.androidhotelbooking;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RoomsActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room);
+    }
+}
