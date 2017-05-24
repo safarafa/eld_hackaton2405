ca2a4483068821fda9c9a577706ae6b8bb1c6759
Jakub Śledzik <jaksledzik@gmail.com>
2017-05-24 14:46:06 +0200

    fixed bugs and improve performance

--- bookify-angular/src/app/summarize/summarize.component.ts
+++ bookify-angular/src/app/summarize/summarize.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-summarize',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SummarizeComponent implements OnInit {
 
-  constructor() { }
+  rooms: number = 0;
+  people: number = 0;
+  price: number = 0;
+  location: String = '';
+  hotel: String = '';
+
+  constructor(private router: Router,
+  private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.rooms = this.route.snapshot.params['rooms'];
+    this.people = this.route.snapshot.params['people'];
+    this.price = this.route.snapshot.params['price'];
+    this.location = this.route.snapshot.params['location'];
+    this.hotel = this.route.snapshot.params['hotel'];
   }
 
 }
