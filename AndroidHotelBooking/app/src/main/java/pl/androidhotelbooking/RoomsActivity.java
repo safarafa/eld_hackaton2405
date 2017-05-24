1677f76f77f86d4f032d599701c52385fd63bc40
Unknown <SylwekMuzyka@gmail.com>
2017-05-24 13:43:10 +0200

    android layout 3

--- AndroidHotelBooking/app/src/main/java/pl/androidhotelbooking/RoomsActivity.java
+++ AndroidHotelBooking/app/src/main/java/pl/androidhotelbooking/RoomsActivity.java
@@ -1,15 +1,28 @@
 package pl.androidhotelbooking;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class RoomsActivity extends Activity
 {
+    public static final String SELECTED_ROOM = "selected_room";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_room);
+        setContentView(R.layout.activity_rooms);
+
+        int numberOfPeople = getIntent().getExtras().getInt(MainActivity.NUMBER_OF_PEOPLE);
+
+
+    }
+
+    private void goToSelectedRoom(int position)
+    {
+        Intent selectedRoom = new Intent(this, SelectedRoomActivity.class);
+        selectedRoom.putExtra(SELECTED_ROOM, position);
+        startActivity(selectedRoom);
     }
 }
