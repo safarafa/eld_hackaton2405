4e2b9ff76a8d9fa402afe0a7127e0e6614d7db29
Konrad Buczkowski <el.konradbuczkowski@gmail.com>
2017-05-24 15:19:57 +0200

    Update hotels.sql

--- hotels.sql
+++ hotels.sql
@@ -1,71 +1,110 @@
--- MySQL Workbench Forward Engineering
+CREATE DATABASE  IF NOT EXISTS `hotels` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `hotels`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: hotels
+-- ------------------------------------------------------
+-- Server version	5.7.17-log
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema hotels
--- -----------------------------------------------------
+--
+-- Table structure for table `customers`
+--
 
--- -----------------------------------------------------
--- Schema hotels
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `hotels` DEFAULT CHARACTER SET utf8 ;
-USE `hotels` ;
+DROP TABLE IF EXISTS `customers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customers` (
+  `id_customer` bigint(20) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_customer`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `hotels`.`customers`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `hotels`.`customers` (
-  `id_customer` BIGINT(20) NOT NULL AUTO_INCREMENT,
-  `first_name` VARCHAR(50) NOT NULL,
-  `last_name` VARCHAR(50) NOT NULL,
-  PRIMARY KEY (`id_customer`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 5
-DEFAULT CHARACTER SET = utf8;
+--
+-- Dumping data for table `customers`
+--
 
+LOCK TABLES `customers` WRITE;
+/*!40000 ALTER TABLE `customers` DISABLE KEYS */;
+INSERT INTO `customers` VALUES (1,'Jan','Kowalski'),(2,'Jan','Nowak'),(3,'Piotr','Nowak'),(4,'Krzysztof','Kowalski');
+/*!40000 ALTER TABLE `customers` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- -----------------------------------------------------
--- Table `hotels`.`rooms`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `hotels`.`rooms` (
-  `id_room` BIGINT(20) NOT NULL AUTO_INCREMENT,
-  `price` DOUBLE NOT NULL,
-  `seats` INT(11) NOT NULL,
-  PRIMARY KEY (`id_room`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 5
-DEFAULT CHARACTER SET = utf8;
+--
+-- Table structure for table `reservations`
+--
 
-
--- -----------------------------------------------------
--- Table `hotels`.`reservations`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `hotels`.`reservations` (
-  `id_reservation` BIGINT(20) NOT NULL AUTO_INCREMENT,
-  `description` VARCHAR(255) NULL DEFAULT NULL,
-  `total_price` DOUBLE NOT NULL,
-  `id_customer` BIGINT(20) NULL DEFAULT NULL,
-  `id_room` BIGINT(20) NULL DEFAULT NULL,
+DROP TABLE IF EXISTS `reservations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `reservations` (
+  `id_reservation` bigint(20) NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) DEFAULT NULL,
+  `total_price` double NOT NULL,
+  `id_customer` bigint(20) DEFAULT NULL,
+  `id_room` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id_reservation`),
-  INDEX `FKofp1thcwrrp6bvadtj6028dpe` (`id_customer` ASC),
-  INDEX `FKigg5lvd5cn7p77h56gwsg0opq` (`id_room` ASC),
-  CONSTRAINT `FKigg5lvd5cn7p77h56gwsg0opq`
-    FOREIGN KEY (`id_room`)
-    REFERENCES `hotels`.`rooms` (`id_room`),
-  CONSTRAINT `FKofp1thcwrrp6bvadtj6028dpe`
-    FOREIGN KEY (`id_customer`)
-    REFERENCES `hotels`.`customers` (`id_customer`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 5
-DEFAULT CHARACTER SET = utf8;
+  KEY `FKofp1thcwrrp6bvadtj6028dpe` (`id_customer`),
+  KEY `FKigg5lvd5cn7p77h56gwsg0opq` (`id_room`),
+  CONSTRAINT `FKigg5lvd5cn7p77h56gwsg0opq` FOREIGN KEY (`id_room`) REFERENCES `rooms` (`id_room`),
+  CONSTRAINT `FKofp1thcwrrp6bvadtj6028dpe` FOREIGN KEY (`id_customer`) REFERENCES `customers` (`id_customer`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `reservations`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+LOCK TABLES `reservations` WRITE;
+/*!40000 ALTER TABLE `reservations` DISABLE KEYS */;
+INSERT INTO `reservations` VALUES (1,'Opis1',100,1,1),(2,'Opis2',200,2,2),(3,'Opis3',300,3,3),(4,'Opis1',100,1,1);
+/*!40000 ALTER TABLE `reservations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rooms`
+--
+
+DROP TABLE IF EXISTS `rooms`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `rooms` (
+  `id_room` bigint(20) NOT NULL AUTO_INCREMENT,
+  `price` double NOT NULL,
+  `seats` int(11) NOT NULL,
+  PRIMARY KEY (`id_room`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rooms`
+--
+
+LOCK TABLES `rooms` WRITE;
+/*!40000 ALTER TABLE `rooms` DISABLE KEYS */;
+INSERT INTO `rooms` VALUES (1,100,1),(2,200,2),(3,300,3),(4,400,4);
+/*!40000 ALTER TABLE `rooms` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-05-24 15:19:27
